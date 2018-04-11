a6eec05cd67ac517970208b43b798d3646910af3
Wilhelm Behncke <2522299+grebaldi@users.noreply.github.com>
2018-04-11 13:19:45 +0200

    FEATURE: Create InterpolatorTest

---
+++ Tests/Unit/InterpolatorTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace PackageFactory\AtomicFusion\Constants\Tests\Unit;
+
+use Neos\Flow\Tests\UnitTestCase;
+use PackageFactory\AtomicFusion\Constants\Interpolator;
+
+class InterpolatorTest extends UnitTestCase
+{
+	/**
+     * @var Parser
+     */
+    protected $constantsInterpolator;
+
+    public function setUp()
+    {
+        $this->constantsInterpolator = new Interpolator();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReplaceConstantPlaceholderWithConstantValue()
+    {
+        $fixture = file_get_contents(__DIR__ . '/../Fixtures/interpolation.fusion');
+        $constants = [
+            'MY_CONSTANT' => 'resource://Neos.Demo/Test'
+        ];
+
+        $result = $this->constantsInterpolator->replaceConstants($fixture, $constants);
+
+        $this->assertContains('path = ${"resource://Neos.Demo/Test" + \'/foo.md\'}', $result);
+    }
+}
